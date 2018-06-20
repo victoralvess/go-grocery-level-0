dcccf423f2235eff836857f2b11f605900e24e3a
Victor Alves <vsilva.programador@gmail.com>
2018-06-19 21:33:20 -0300

    implement error

--- grocery/product/product.go
+++ grocery/product/product.go
@@ -1,5 +1,7 @@
 package product
 
+import "errors"
+
 type Product struct {
 	Name	string	
 	Price	float32
@@ -9,4 +11,15 @@ type Product struct {
 func New(name string, price float32, inStock int) Product {
 	p := Product { name, price, inStock }
 	return p
+}
+
+func (product *Product) IsAvailableInStock() (available bool, err error) {
+	available = product.InStock > 0
+	if available {
+		err = nil
+	} else {
+		err = errors.New(product.Name + " is out of stock")
+	}
+
+	return
 }
