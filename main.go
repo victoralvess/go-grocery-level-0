fd200cb8b0eb3f1afd80afac3df0cae7d9ad6611
Victor Alves <vsilva.programador@gmail.com>
2018-06-20 12:07:36 -0300

    format

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/victoralvess/go-grocery-level-0/grocery/product"
+
 	"github.com/victoralvess/go-grocery-level-0/grocery/customer"
+	"github.com/victoralvess/go-grocery-level-0/grocery/product"
 )
 
 func main() {
 	var egg = product.New("Egg", 2.0, 50)
 	var milk = product.New("Milk", 5.0, 20)
 	var outOfStock = product.New("Out of Stock", 100000000, 0)
-	
+
 	var customer = customer.New()
 
 	customer.AddToCart(egg)
-    customer.AddToCart(egg)
-    customer.AddToCart(egg)
-    customer.AddToCart(milk)
-    customer.AddToCart(outOfStock)
+	customer.AddToCart(egg)
+	customer.AddToCart(egg)
+	customer.AddToCart(milk)
+	customer.AddToCart(outOfStock)
 
 	fmt.Printf("Total: $%v\n", customer.Buy())
 	fmt.Printf("Total (Empty Cart): $%v\n", customer.Buy())
