1fa1fdfe0c49211323231f7aacf7e159b9ede76a
Victor Alves <vsilva.programador@gmail.com>
2018-06-20 12:18:13 -0300

    format

--- grocery/product/product.go
+++ grocery/product/product.go
@@ -2,17 +2,20 @@ package product
 
 import "errors"
 
+// Product defines a product
 type Product struct {
-	Name	string	
-	Price	float32
-	InStock	int
+	Name    string
+	Price   float32
+	InStock int
 }
 
+// New creates a new product.
 func New(name string, price float32, inStock int) Product {
-	p := Product { name, price, inStock }
+	p := Product{name, price, inStock}
 	return p
 }
 
+// IsAvailableInStock verifies if the product is available in stock.
 func (product *Product) IsAvailableInStock() (available bool, err error) {
 	available = product.InStock > 0
 	if available {
